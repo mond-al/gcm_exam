501d2c37aa383cd33fe1e85a1d3e0050175dcb22
양지수 <zzisoo9@uangel.com>
2015-09-10 11:44:53 +0900

    Implement sendRegistrationToServer Add Retrofit (by gradle)

---
+++ app/src/main/java/com/gongdol/http/Contants.java
@@ -0,0 +1,8 @@
+package com.gongdol.http;
+
+/**
+ * Created by yangjisoo on 15. 9. 10..
+ */
+public class Contants {
+    public static final String SERVER_URL = "http://api.test.com";
+}
---
+++ app/src/main/java/com/gongdol/http/RequestInterface.java
@@ -0,0 +1,32 @@
+package com.gongdol.http;
+
+import com.gongdol.http.vo.TokenRegResponse;
+
+import retrofit.Callback;
+import retrofit.http.POST;
+import retrofit.http.Path;
+
+/**
+ * Created by Gongdol ZZisoo on 15. 9. 10..
+ */
+
+
+public interface RequestInterface {
+
+    /**
+     * RequestInterface gcm token resisteration 3rd party Server with UserID
+     * Callback implement by TokenRegResponse class
+     *
+     * @param userId
+     * @param token
+     * @param callback
+     *
+     */
+    @POST("/gcm/registeration/toekn/{userId}/{token}")
+    void GcmTokenReg(
+            @Path("userId") String userId,
+            @Path("token") String token,
+            Callback<TokenRegResponse> callback
+    );
+
+}
